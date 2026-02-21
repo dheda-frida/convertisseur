b6995f9227a8305111a8e931dac80f7c69f6bdc4
dheda-frida <fridaketya02@icloud.com>
2026-02-21 11:12:05 +0100

    premier commit - convertisseur rpmain arabe

---
+++ bin/maindart.dart
@@ -0,0 +1,5 @@
+import 'package:maindart/maindart.dart' as maindart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${maindart.calculate()}!');
+}
---
+++ test/maindart_test.dart
@@ -0,0 +1,8 @@
+import 'package:maindart/maindart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
